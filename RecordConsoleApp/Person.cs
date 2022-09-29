d9b7ac333a7122ae0b403df77baf918033645556
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2022-09-29 14:26:11 +0200

    Add project files.

---
+++ RecordConsoleApp/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordConsoleApp
+{
+
+    public record Teacher(string firstname, string lastname, string grade) :
+        Person(firstname, lastname);
+    public record Person(string Firstname, string Lastname)
+    {
+        //public int MyProperty { get; set; }
+    }
+}
---
+++ RecordConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RecordConsoleApp;
+
+Console.WriteLine("Hello, World!");
+
+Person p = new ("Jens", "Jensen");
+
+Person p1 = new Person("Jens", "Jensen");
+
+Console.WriteLine(p);
+Console.WriteLine(p1);
+
+if (p == p1)
+    Console.WriteLine("disse to record er ens");
+else
+    Console.WriteLine("Disse to record er ikke ens");
+
+//With
+Person p3 = p1 with { Firstname = "Søren" };
+
+Console.WriteLine(p3);
